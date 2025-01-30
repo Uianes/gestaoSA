c470466c024072711307e47932ca41652251660f
Arthur Willers <arthur2008willers@gmail.com>
2025-01-30 19:43:51 -0300

    Refactor database connection functions to set charset to utf8

--- db_connection.php
+++ db_connection.php
@@ -1,18 +1,22 @@
 <?php
+function open_connection() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "gestaosa";
 
-function open_connection()
-{
-    $server_name = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'gestaosa';
-    $conn = mysqli_connect($server_name, $username, $password, $dbname) or die("Connect failed: %s\n". mysqli_error($conn));
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    mysqli_set_charset($conn, "utf8");
+
     return $conn;
 }
 
-function close_connection($conn)
-{
+function close_connection($conn) {
     mysqli_close($conn);
 }
-
 ?>
