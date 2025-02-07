5a4bd404965312774354df270863c4b1f0892fa4
Arthur Willers <arthur2008willers@gmail.com>
2025-02-07 13:21:19 -0300

    Change error handling for school selection to use alert and close window

--- pdf.php
+++ pdf.php
@@ -2,8 +2,7 @@
 include './db_connection.php';
 
 if (!isset($_POST['locais']) || empty($_POST['locais'])) {
-    echo "<div class='alert alert-danger'>Nenhuma escola selecionada.</div>";
-    header('Refresh: 0.5; URL=./index.php');
+    echo "<script>alert('Nenhuma escola selecionada.'); window.close();</script>";
     exit;
 }
 
