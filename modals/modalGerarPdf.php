114765057ba7dc45d0286cb8da3569c816fb04f0
Arthur Willers <arthur2008willers@gmail.com>
2025-02-07 12:14:03 -0300

    Add Dompdf library

--- modals/modalGerarPdf.php
+++ modals/modalGerarPdf.php
@@ -10,41 +10,41 @@
           <input class="form-check-input" type="checkbox" id="checkAll" onclick="toggleAllCheckboxes(this)">
           <label class="form-check-label" for="checkAll">Selecionar Todas</label>
         </div>
-        <form action="pdf.php" method="POST" onsubmit="return validarCheck()">
+        <form action="pdf.php" method="POST" onsubmit="return validarCheck()" target="_blank">
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="SME" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="SME" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">SME</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEI Pequeno Paraíso" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEI Pequeno Paraíso" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEI Pequeno Paraíso</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEI Vaga-Lume" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEI Vaga-Lume" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEI Vaga-Lume</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEI Vovó Amália" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEI Vovó Amália" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEI Vovó Amália</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEF Sol Nascente" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEF Sol Nascente" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEF Sol Nascente</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEF Rui Barbosa" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEF Rui Barbosa" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEF Rui Barbosa</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEF Antônio João" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEF Antônio João" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEF Antônio João</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEF São João" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEF São João" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEF São João</label>
           </div>
           <div class="form-check">
-            <input class="form-check-input checkSchool" type="checkbox" name="escolas[]" value="EMEF Antônio Liberato" onclick="uncheckMainCheckbox()">
+            <input class="form-check-input checkSchool" type="checkbox" name="locais[]" value="EMEF Antônio Liberato" onclick="uncheckMainCheckbox()">
             <label class="form-check-label">EMEF Antônio Liberato</label>
           </div>
           <div class="mt-3">
--- pdf.php
+++ pdf.php
@@ -1,8 +1,14 @@
-<?php 
-include '../db_connection.php';
+<?php
+include './db_connection.php';
+
+if (!isset($_POST['locais']) || empty($_POST['locais'])) {
+    echo "<div class='alert alert-danger'>Nenhuma escola selecionada.</div>";
+    header('Refresh: 0.5; URL=./index.php');
+    exit;
+}
+
 $conn = open_connection();
+$schools = $_POST['locais'];
+$date = date('d/m/Y H:i:s');
 
-
-close_connection($conn);
-header('Refresh: 0.5; URL=../index.php');
 ?>
