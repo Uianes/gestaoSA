59f1517f849cce712de5a0409f2bbbc218fa6e07
Arthur Willers <arthur2008willers@gmail.com>
2025-03-21 15:41:10 -0300

    Update page title and branding from 'PatrimôniosSME' to 'SGP-SME'

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="./assets/logoPrefeituraSA.png" type="image/x-icon">
-  <title>PatrimôniosSME</title>
+  <title>SGP-SME</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -32,7 +32,7 @@
     <div class="container-fluid d-flex">
       <a class="navbar-brand" href="#">
         <img src="./assets/logoPrefeituraSA.png" alt="Logo Prefeitura" height="30" class="d-inline-block align-text-top">
-        PatrimôniosSME
+        SGP-SME
       </a>
       <button class="btn btn-primary ms-3" type="button" data-bs-toggle="modal" data-bs-target="#ModalCadastrarPatrimonio">
         Cadastrar Patrimônio
